a9c6217a56c2b4a9cb94da68aa8831d0cca2c16d
Palocz Istvan <istvan@palocz.hu>
2021-11-28 06:34:18 +0100

    use automatic repo path setting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+let path = '/'
+if (process.env.GITHUB_REPOSITORY) {
+  path = process.env.GITHUB_REPOSITORY.split('/')[1]
+}
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuebase/'
+    ? path
     : '/'
 }
